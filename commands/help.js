c3fb505c9dd25ab877bd057d7d14a848a9fe209b
Oğuzhan <64753457+OguzhanUmutlu@users.noreply.github.com>
2021-05-07 17:44:58 +0300

    Delete commands directory

--- commands/help.js
+++
@@ -1,8 +0,0 @@
-const { MessageEmbed } = require("discord.js");
-
-module.exports = {
-    name: "yardım",
-    execute: async function(m, args, config){
-        m.reply("Komutlar: "+require("../index").getClient().cmds.map(i=> "`"+i.name+"`").join(", "));
-    }
-}
