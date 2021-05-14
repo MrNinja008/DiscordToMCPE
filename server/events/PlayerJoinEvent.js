b7178d54ee91eebca979981a9f48576d99eb63aa
Oğuzhan <64753457+OguzhanUmutlu@users.noreply.github.com>
2021-05-14 23:57:14 +0300

    pff

--- server/events/PlayerJoinEvent.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(playerName){
-    let config = require("../../config");
-    let rcon = require("../../index").getClient().currentrcon;
-    if (!rcon) return;
-    rcon.send("exrcon getplayers");
-}
