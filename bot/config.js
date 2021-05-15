d9cb8a6f7e2efd9f189ecc3eb1a4f042e1a72e6d
Oğuzhan <64753457+OguzhanUmutlu@users.noreply.github.com>
2021-05-15 17:31:37 +0300

    configi unuttuh

--- bot/config.js
+++ bot/config.js
@@ -7,6 +7,99 @@ module.exports =
     "token": "Discord Tokeninizi Girin",// DEĞİŞTİR
     "prefix": "/",
     "guild": "Sunucu id'nizi girin",// DEĞİŞTİR
+    "logger": {
+        "consoleCommand": {
+            // Konsolda bir komut çalıştığında harekete geçer.
+            // Değişkenler: {command}
+            "enabled": false,
+            "channel": {
+                "guildID": "123456789012345678",
+                "channelID": "123456789012345678"
+            },
+            "message": "Konsolda {command} komudu çalıştırıldı."
+        },
+        "playerchat": {
+            // Bir oyuncu mesaj yazdığında harekete geçer.
+            // Değişkenler: {player}, {message}
+            "enabled": false,
+            "channel": {
+                "guildID": "123456789012345678",
+                "channelID": "123456789012345678"
+            },
+            "message": "{player} > {message}"
+        },
+        "playercommand": {
+            // Bir oyuncu komut yazdığında harekete geçer.
+            // Değişkenler: {player}, {command}
+            "enabled": false,
+            "channel": {
+                "guildID": "123456789012345678",
+                "channelID": "123456789012345678"
+            },
+            "message": "{player} > /{command}"
+        },
+        "playerdeath": {
+            // Bir oyuncu öldüğünde harekete geçer.
+            // Değişkenler: {player}, {killer}
+            "enabled": false,
+            "channel": {
+                "guildID": "123456789012345678",
+                "channelID": "123456789012345678"
+            },
+            "messages": {
+                "byPlayer": "{player}, {killer} tarafından öldürüldü.",
+                "forReason": [
+                    "{player} bir şeye dokunarak öldü.",
+                    "{player} canlı tarafından öldürüldü.",
+                    "{player} vuruldu.",
+                    "{player} bloğun içinde sıkışarak öldü.",
+                    "{player} yere düşerek öldü.",
+                    "{player} ateşte öldü.",// ateşin içindeyken ölme
+                    "{player} yanarak öldü.",// ateşten veya lavdan çıkıp kalan ateş yüzünden ölme
+                    "{player} lavda yüzebileceğini sandı.",
+                    "{player} boğularak öldü.",
+                    "{player} blok tarafından patlatıldı.",
+                    "{player} canlı tarafından patlatıldı.",//tnt, ateş topu vs.
+                    "{player} boşluğa düşerek öldü.",
+                    "{player} intihar etti :(",
+                    "{player} büyü ile öldürüldü.",
+                    "{player} gizemli bir yolla öldürüldü?!",
+                    "{player} açlıktan öldü."
+                ],
+                "nothing": "{player} öldü."
+            }
+        },
+        "playerjoin": {
+            // Bir oyuncu sunucuya katıldığında harekete geçer.
+            // Değişkenler: {player}
+            "enabled": false,
+            "channel": {
+                "guildID": "123456789012345678",
+                "channelID": "123456789012345678"
+            },
+            "message": "{player} sunucuya katıldı."
+        },
+        "playerquit": {
+            // Bir oyuncu sunucudan ayrıldığında harekete geçer.
+            // Değişkenler: {player}
+            "enabled": false,
+            "channel": {
+                "guildID": "123456789012345678",
+                "channelID": "123456789012345678"
+            },
+            "message": "{player} sunucudan ayrıldı."
+        },
+        "rconcommand": {
+            // RCON'dan komut gönderildiğinde harekete geçer.
+            // Değişkenler: {command}
+            "enabled": false,
+            "channel": {
+                "guildID": "123456789012345678",
+                "channelID": "123456789012345678"
+            },
+            "message": "{command} komudu uzaktan çalıştırıldı."
+        }
+    },
     "talepsistem": {
         "enabled": true,
         "quitclose": true,// Oyuncu oyundan çıkınca talep kapatılsın mı?
