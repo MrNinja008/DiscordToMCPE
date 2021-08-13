9e1535bd27a9c7f5c72afb3cd8df99f25ef6294e
MrNinja008 <79526396+MrNinja008@users.noreply.github.com>
2021-08-13 21:55:34 +0530

    Translate

--- bot/commands/banlist.js
+++ bot/commands/banlist.js
@@ -1,12 +1,12 @@
 module.exports = {
-    name: "banliste",
+    name: "ban list",
     execute: async function (m, args, config) {
-        if (!m.member.hasPermission("ADMINISTRATOR")) return m.reply("Bu komudu kullanabilmek için `Yönetici` olman gerekiyor.");
-        if (!config.server.rcon.enabled) return m.reply("Rcon devre dışı bırakıldığından bu komudu kullanamazsınız.");
+        if (!m.member.hasPermission("ADMINISTRATOR")) return m.reply("You must be an 'Administrator' to use this command.");
+        if (!config.server.rcon.enabled) return m.reply("Rcon You cannot use this command because it is disabled.");
         let rcon = require("../index").getClient().currentrcon;
         if (!rcon) return m.reply("Rcon bulunamadı.");
         let result = await Promise.all([rcon.send("banlist " + (args[0] || "players"))]);
-        let bannedplayers = result[0].split("\r\n")[1] || "Kimse";
-        await m.reply("Sunucudan uzaklaştırılmış oyuncular: " + bannedplayers);
+        let bannedplayers = result[0].split("\r\n")[1] || "Noone";
+        await m.reply("Suspended players: " + bannedplayers);
     }
 }
